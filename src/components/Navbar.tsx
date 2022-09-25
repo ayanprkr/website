8ce99fb8dfdb624469f3eb1dd56abaa8b350e72f
MetalOoze <38457291+MetalOoze05@users.noreply.github.com>
2022-09-25 15:21:48 +0530

    fix: layout of the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,6 +30,18 @@ const Navbar: FC<Props> = ({ path }) => {
             </button>
         </>
     )
+
+    const navLinksGuestbook = (
+        <>
+            <button onClick={(e) => handleClick(e, "/")} className="hover:bg-neutral-900 text-gray-500 rounded-lg transition duration-200 px-4 py-2">
+                <Link href="/">/</Link>
+            </button>
+            <button onClick={(e) => handleClick(e, "/guestbook")} className="bg-neutral-900 hover:bg-neutral-800 text-gray-300 rounded-lg transition duration-200 px-4 py-2">
+                <Link href="/guestbook">/guestbook</Link>    
+            </button>
+        </>
+    )
+
     if (path === "/") {
         return (
             <header className="sticky md:relative flex justify-between items-center w-full top-2 md:top-0 gap-5">
@@ -45,14 +57,12 @@ const Navbar: FC<Props> = ({ path }) => {
         )
     } else if (path === "/guestbook") {
         return (
-            <header className="pt-10 px-14 md:px-20 flex flex-wrap justify-between items-center relative w-full top-0 gap-5">
-                <nav className="flex gap-5">
-                    <button onClick={(e) => handleClick(e, "/")} className="hover:bg-neutral-900 text-gray-500 rounded-lg transition duration-200 px-4 py-2">
-                        <Link href="/">/</Link>
-                    </button>
-                    <button onClick={(e) => handleClick(e, "/guestbook")} className="bg-neutral-900 hover:bg-neutral-800 text-gray-300 rounded-lg transition duration-200 px-4 py-2">
-                        <Link href="/guestbook">/guestbook</Link>    
-                    </button>
+            <header className="sticky md:relative flex justify-between items-center w-full top-2 md:top-0 gap-5">
+                <div className="visible md:hidden bg-neutral-900 bg-opacity-50 backdrop-blur-md rounded-lg text-gray-500">
+                    <Hamburger size={20} toggled={isOpen} toggle={setOpen} rounded />
+                </div>
+                <nav className="hidden md:flex gap-5">
+                    {navLinksGuestbook}
                 </nav>
 
                 <SpotifyWidget />
@@ -61,18 +71,16 @@ const Navbar: FC<Props> = ({ path }) => {
     }
 
     return (
-        <header className="pt-10 px-14 md:px-20 flex flex-wrap justify-between items-center relative w-full top-0 gap-5">
-                <nav className="flex gap-5">
-                    <button onClick={(e) => handleClick(e, "/")} className="bg-neutral-900 hover:bg-neutral-800 text-gray-300 rounded-lg transition duration-200 px-4 py-2">
-                        <Link href="/">/</Link>
-                    </button>
-                    <button onClick={(e) => handleClick(e, "/guestbook")} className="hover:bg-neutral-900 text-gray-500 rounded-lg transition duration-200 px-4 py-2">
-                        <Link href="/guestbook">/guestbook</Link>    
-                    </button>
-                </nav>
+        <header className="sticky md:relative flex justify-between items-center w-full top-2 md:top-0 gap-5">
+            <div className="visible md:hidden bg-neutral-900 bg-opacity-50 backdrop-blur-md rounded-lg text-gray-500">
+                <Hamburger size={20} toggled={isOpen} toggle={setOpen} rounded />
+            </div>
+            <nav className="hidden md:flex gap-5">
+                {navLinks}
+            </nav>
 
-                <SpotifyWidget />
-            </header>
+            <SpotifyWidget />
+        </header>
     )
 }
 
